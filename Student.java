f0e1c63b11b15b9dcf3a814c291eae7acd370fcf
Aleksandar Tasevski <aleksandar_tasevski@outlook.com>
2020-04-12 13:44:58 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -5,15 +5,54 @@ class Student {
 	
 	List<Integer> labPoints;
 
-	//TODO constructor
-
-	//TODO seters & getters
+	public Student(String index; String firstName; String lastName, List<Integer> labPoints) {
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.labPoints = labPoints;
+	}
+	
+	public String getIndex(){
+		return index;
+	}
+	
+	public String getFirstName(){
+		return firstName;
+	}
+	
+	public String getLastName(){
+		return lastName;
+	}
+	
+	public List<Student> getLabPoints(){
+		return labPoints;
+	}
+	
+	public void setIndex(String index){
+		this.index = index;
+	}
+	
+	public void setFirstName(String FirstName){
+		this.firstName = firstName;
+	}
+	
+	public void setLastName(String LastName){
+		this.lastName = lastName;
+	}
+	
+	public void setLabPoints(List<Integer> LabPoints){
+		this.labPoints = labPoints;
+	}
 
 	public double getAverage() {
-		//TODO
+		double avg = 0;
+		for(Integer lab : labPoints){
+			avg += lab;
+		}
+		return avg / labPoints.size();
 	}
 
 	public boolean hasSignature() {
-		//TODO
+		return labPoints.size() > 8;
 	}
 }
